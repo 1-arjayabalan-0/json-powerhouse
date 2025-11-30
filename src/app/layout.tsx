a5c66937256c8a6dda8bf23ad25cae18d42946e8
1-arjayabalan-0 <arjayabalan09@gmail.com>
2025-11-30 23:40:09 +0530

    analytics, speed added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 import SideNav from "./components/SideNav";
 import { ThemeProvider } from "./components/ThemeProvider";
 import AlertService from "./components/alert-service";
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -51,6 +52,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         </link>
         {/* Google Fonts  */}
+
+        <Analytics />
+        <SpeedInsights />
 
       </head>
       <body className="flex bg-gray-950 text-gray-100">
